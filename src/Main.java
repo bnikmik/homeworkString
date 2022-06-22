b2a145be74518b798afbed17b69c5a4d65094497
Nikita Bolshakov <bnikmik@gmail.com>
2022-06-22 18:10:39 +0300

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание №1
+        System.out.println("Задание №1");
+
+        String firstName = "Семён ";
+        String middleName = "Семёнович ";
+        String lastName = "Иванов ";
+
+        String fullName  = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println(" ");
+
+        //Задание №2
+        System.out.println("Задание №2");
+
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUpper);
+        System.out.println(" ");
+
+        //Задание №3
+        System.out.println("Задание №3");
+
+        String fullNameRight = fullName.replace('ё', 'e');
+        System.out.println("Данные ФИО сотрудника — " + fullNameRight);
+
+
     }
 }
